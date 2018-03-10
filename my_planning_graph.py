d6a203f45daa9327650fb05a140e9b703bffe65e
Hoang Chung Hien <hoangchunghien@gmail.com>
2018-03-10 15:49:18 +0700

    feat: Add code to negation_mutex

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -465,8 +465,7 @@ class PlanningGraph():
         :param node_s2: PgNode_s
         :return: bool
         """
-        # TODO test for negation between nodes
-        return False
+        return node_s1.symbol == node_s2.symbol and node_s1.is_pos != node_s2.is_pos
 
     def inconsistent_support_mutex(self, node_s1: PgNode_s, node_s2: PgNode_s):
         """
