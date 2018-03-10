9dd83520b48fbee76ecc79b0d1a7db544662fcff
Hoang Chung Hien <hoangchunghien@gmail.com>
2018-03-10 16:36:16 +0700

    feat: Add code for inconsistent_support_mutex

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -495,8 +495,11 @@ class PlanningGraph():
         :param node_s2: PgNode_s
         :return: bool
         """
-        # TODO test for Inconsistent Support between nodes
-        return False
+        for a1 in node_s1.parents:
+            for a2 in node_s2.parents:
+                if not a1.is_mutex(a2):
+                    return False
+        return True
 
     def h_levelsum(self) -> int:
         """The sum of the level costs of the individual goals (admissible if goals independent)
